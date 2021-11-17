8a3622a40c10e21e8a4a91e49b3e256a58cad50d
Lawrence C <52389524+rsvp-lawrencec@users.noreply.github.com>
2021-11-17 21:40:21 +0800

    Fixed Custom Application class and MainActivity package path

--- app/src/main/java/hk/com/lolamove/app/MainActivity.kt
+++ app/src/main/java/hk/com/lolamove/app/MainActivity.kt
@@ -1,11 +1,14 @@
-package hk.com.lolamove
+package hk.com.lolamove.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hk.com.lolamove.app.R
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.d("onCreate() -> MainActivity")
         setContentView(R.layout.activity_main)
     }
 }
